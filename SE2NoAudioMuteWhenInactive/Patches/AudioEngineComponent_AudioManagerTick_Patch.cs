ad0fbfb6b9dcad7b0a7bec48293a3bc98d46e1cd
WesternGamer <Kevnguyen474@gmail.com>
2025-01-29 20:23:15 -0500

    Add project files.

---
+++ SE2NoAudioMuteWhenInactive/Patches/AudioEngineComponent_AudioManagerTick_Patch.cs
@@ -0,0 +1,30 @@
+﻿using HarmonyLib;
+using Keen.VRage.Audio.EngineComponents;
+using System.Reflection.Emit;
+using System.Reflection;
+using Keen.VRage.Library.Extensions;
+
+namespace SE2NoAudioMuteWhenInactive.Patches
+{
+    [HarmonyPatch(typeof(AudioEngineComponent), "AudioManagerTick")]
+    internal class AudioEngineComponent_AudioManagerTick_Patch
+    {
+        private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            var il = instructions.ToList();
+
+            int foundIndex = il.FindIndex(i => i.opcode == OpCodes.Callvirt && (i.operand as MethodInfo) == AccessTools.Method("Keen.VRage.Audio.AudioManager:Update"));
+
+            object audManagerInstanceGetterOperand = il[foundIndex - 1].operand;
+
+            List<CodeInstruction> newIl =
+                [
+                new CodeInstruction(OpCodes.Call, audManagerInstanceGetterOperand),
+                new CodeInstruction(OpCodes.Callvirt, AccessTools.Method("Keen.VRage.Audio.AudioManager:Update")),
+                new CodeInstruction(OpCodes.Ret)
+                ];
+
+            return newIl;
+        }
+    }
+}
---
+++ SE2NoAudioMuteWhenInactive/Plugin.cs
@@ -0,0 +1,24 @@
+﻿using HarmonyLib;
+using Keen.Game2.Game.Plugins;
+using Keen.VRage.Library.Diagnostics;
+using System.Reflection;
+
+namespace SE2NoAudioMuteWhenInactive
+{
+    public class Plugin : IPlugin
+    {
+        public const string Name = "SE2NoAudioMuteWhenInactive";
+
+        public Plugin()
+        {
+            Log.Default.WriteLine($"[{Name}] Loaded plugin.");
+#if DEBUG
+            Harmony.DEBUG = true;
+#endif
+            Harmony harmony = new Harmony(Name);
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+
+            Log.Default.WriteLine($"[{Name}] Applied patches");
+        }
+    }
+}
